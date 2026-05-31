3b7454a918278ec011c0bef0a70b61bdacd13aa0
RK M <77720246+rk6789@users.noreply.github.com>
2026-06-01 00:52:37 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Bump VERSION on every release so clients fetch the new files.
+const VERSION = '1.0.0';
+const CACHE = 'eggcounter-v' + VERSION;
+const CORE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(CORE.map(u => c.add(u)))));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+// cache-first, then network; runtime-cache fonts and other GETs
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => { try { c.put(req, copy); } catch (_) {} });
+      return res;
+    }).catch(() => hit))
+  );
+});
