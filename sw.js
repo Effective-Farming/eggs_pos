1680ed87f1584a416c7f84bededa35fd42c85515
RK M <77720246+rk6789@users.noreply.github.com>
2026-06-03 16:50:36 +0530

    v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump VERSION on every release so clients fetch the new files.
-const VERSION = '1.0.0';
+const VERSION = '1.2.0';
 const CACHE = 'eggcounter-v' + VERSION;
 const CORE = [
   './',
